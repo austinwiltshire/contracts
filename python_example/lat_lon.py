624a6194a7f1eb2a44a2e773cf3de5408068b1e1
John Graham <john@simpli.fi>
2015-12-29 14:58:57 -0600

    to pre and post conditions

--- python_example/lat_lon.py
+++ python_example/lat_lon.py
@@ -1,11 +1,13 @@
 def new_location(old_latitude, old_longitude, latitude_delta, longitude_delta):
+	#preconditions
+	assert 90 >= old_latitude >= -90, "Must be between 90 and -90 degrees"
+	assert 180 >= old_longitude >= -180, "Must be between 90 and -90 degrees"
 
-	#process invariants
 	new_latitude = old_latitude + latitude_delta
-	assert 90 >= new_latitude >= -90, "Must be between 90 and -90 degrees"
-	
 	new_longitude = old_longitude + longitude_delta
-	assert 180 >= new_longitude >= -180, "Must be between 180 and -180 degrees"
 
+	#postconditions
+	assert 90 >= new_latitude >= -90, "Must be between 90 and -90 degrees"
+	assert 180 >= new_longitude >= -180, "Must be between 180 and -180 degrees"
 	return new_latitude, new_longitude
 	
