457a1174f135eff3a4495b97c61ae5b8090bf2af
John Graham <john@simpli.fi>
2015-12-29 14:55:02 -0600

    added process invariants

--- python_example/lat_lon.py
+++ python_example/lat_lon.py
@@ -1,2 +1,11 @@
 def new_location(old_latitude, old_longitude, latitude_delta, longitude_delta):
-	return old_latitude + latitude_delta, old_longitude + longitude_delta
+
+	#process invariants
+	new_latitude = old_latitude + latitude_delta
+	assert 90 >= new_latitude >= -90, "Must be between 90 and -90 degrees"
+	
+	new_longitude = old_longitude + longitude_delta
+	assert 180 >= new_longitude >= -180, "Must be between 180 and -180 degrees"
+
+	return new_latitude, new_longitude
+	
