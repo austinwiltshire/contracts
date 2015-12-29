f2fe1887f9ad0135b2ed7626f57d134373ff337c
John Graham <john@simpli.fi>
2015-12-29 15:09:55 -0600

    Preconditions to object invariants

--- python_example/lat_lon.py
+++ python_example/lat_lon.py
@@ -1,13 +1,36 @@
+
+class Latitude(object):
+	def __init__(self, value):
+		self._value = value
+		self._invariant_()
+
+	def _invariant_(self):
+		assert 90 >= self._value >= -90, "Must be between 90 and -90 degrees"
+
+	def __add__(self, other):
+		self._invariant_()
+		return Latitude(self._value + other._value)
+
+class Longitude(object):
+	def __init__(self, value):
+		self._value = value
+		self._invariant_()
+
+	def _invariant_(self):
+		assert 180 >= self._value >= -180, "Must be between 180 and -180 degrees"
+
+	def __add__(self, other):
+		self._invariant_()
+		return Longitude(self._value + other._value)
+
+
 def new_location(old_latitude, old_longitude, latitude_delta, longitude_delta):
-	#preconditions
-	assert 90 >= old_latitude >= -90, "Must be between 90 and -90 degrees"
-	assert 180 >= old_longitude >= -180, "Must be between 90 and -90 degrees"
+	#type checking not required, just here to illustrate the object invariant.
+	# In statically typed languages this wouldn't be required at all
+	assert isinstance(old_latitude, Latitude) and isinstance(latitude_delta, Latitude)
+	assert isinstance(old_longitude, Longitude) and isinstance(longitude_delta, Longitude)
 
 	new_latitude = old_latitude + latitude_delta
 	new_longitude = old_longitude + longitude_delta
 
-	#postconditions
-	assert 90 >= new_latitude >= -90, "Must be between 90 and -90 degrees"
-	assert 180 >= new_longitude >= -180, "Must be between 180 and -180 degrees"
 	return new_latitude, new_longitude
-	
